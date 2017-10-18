f65ca6c1443a32a7f3a1c856956a75de5462d145
Janez Pintar <pintarj.zero@gmail.com>
2017-10-18 21:55:30 +0200

    created a system for updating/drawing stuff based on the concept of the Scene

---
+++ script/core.js
@@ -0,0 +1,89 @@
+
+/**
+ * The canvas on which the drawings will be performed. Variable assigned during the call of the boot() function.
+ * @type {HTMLCanvasElement}
+ * */
+var the_canvas = null;
+
+/**
+ * The WebGL rendering context. Variable assigned during the call of the boot() function.
+ * @type {WebGLRenderingContext}
+ * */
+var gl = null;
+
+/**
+ * The last timestamp passed as argument to the loop() method. The unit is milliseconds.
+ * @type {number}
+ * */
+var previous_loop_timestamp = null;
+
+/**
+ * The current scene to update/draw during the frame rendering requests.
+ * @type {Scene}
+ * */
+var current_scene = null;
+
+/**
+ * Update and draw the scene. This method will recursively call itself every time that a frame rendering is required by
+ * the browser. This method is firstly called by the boot() method.
+ * @param {number} timestamp - The timestamp of the frame rendering request.
+ * @return {undefined}
+ * */
+function loop(timestamp) {
+    try {
+        if (!previous_loop_timestamp)
+            previous_loop_timestamp = timestamp - 1000.0 / 60.0;
+
+        var delta = (timestamp - previous_loop_timestamp) / 1000.0;
+        current_scene.update(delta);
+        current_scene.draw();
+        previous_loop_timestamp = timestamp;
+        window.requestAnimationFrame(loop);
+    } catch (exception) {
+        console.error(exception.message);
+    }
+}
+
+/**
+ * Resize the canvas and WebGL context viewport according to the dimension of the window.
+ * @return {undefined}
+ * */
+function on_resize() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    the_canvas.width  = w;
+    the_canvas.height = h;
+    gl.viewport(0, 0, w, h);
+
+    if (current_scene)
+        current_scene.on_resize(w, h);
+}
+
+/**
+ * Returns the initial scene.
+ * @return {Scene}
+ * */
+function create_initial_scene() {
+    return new Scene("base");
+}
+
+/**
+ * Initialize the application. This mean: WebGL context creation, launch the update/draw loop, ...
+ * @return {undefined}
+ * */
+function boot() {
+    try {
+        the_canvas = document.getElementById("the_canvas");
+        gl = the_canvas.getContext("webgl") || the_canvas.getContext("webgl-experimental");
+        on_resize();
+
+        window.addEventListener("resize", function(event) {
+            on_resize();
+        });
+
+        current_scene = create_initial_scene();
+        window.requestAnimationFrame(loop);
+    } catch (exception) {
+        console.error(exception.message);
+    }
+}
---
+++ script/scene.js
@@ -0,0 +1,36 @@
+
+/**
+ * Create a Scene object.
+ * @param {string} id - The scene identifier, should be a string like "example.awesome-scene".
+ * */
+function Scene(id) {
+    this.id = id;
+}
+
+/**
+ * Update the scene.
+ * @param {number} delta - The amount of time to update (units in seconds).
+ * @return {undefined}
+ * */
+Scene.prototype.update = function (delta) {
+    // Default implementation does nothing.
+};
+
+/**
+ * Draw the scene.
+ * @return {undefined}
+ * */
+Scene.prototype.draw = function () {
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+};
+
+/**
+ * Called when the canvas is change size.
+ * @param {number} width - The new width of the canvas.
+ * @param {number} height - The new height of the canvas.
+ * @return {undefined}
+ * */
+Scene.prototype.on_resize = function (width, height) {
+    // Default implementation does nothing.
+};
