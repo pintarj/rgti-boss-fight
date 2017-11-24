45bff4a7cea267e91fa7743204c80b25acfaedbe
Kikot <kb5710@student.uni-lj.si>
2017-11-24 17:52:14 +0100

    Dodana glasba za ozadje in zvok korakov, zvok laserja še ni dokončan

--- script/game/game-scene.js
+++ script/game/game-scene.js
@@ -7,6 +7,24 @@
 //Global variable of starting time
 var startTime;
 
+//Audio context
+//footsteps
+var footstepSoundEffect = new Audio("asset/footStepSoundEffect.mp3");
+const smallNum1 = 0.0;
+const smallNum2 = 0.085;
+const timeDivider = 200; // the smaller the divider, the shorter the steps
+//laser
+var laserSoundEffect = new Audio("asset/laserSoundEffect.mp3");
+laserSoundEffect.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+laserSoundEffect.play();
+//background
+var backgroundMusic = new Audio("asset/glasba2.mp3");
+backgroundMusic.play();
+
+
 /**
  * Create a GameScene object.
  * */
@@ -138,7 +156,16 @@ GameScene.prototype.update = function (delta) {
             break;
         }
     }
-    /*console.log(aJePillarHit);
+    /*console.log(aJePillarHit);*/
+
+
+    //checking if it is time to play the footstep sound
+    if((this.wasd[0] || this.wasd[1] || this.wasd[2] || this.wasd[3]) &&
+     (Math.cos(difTime/timeDivider) < smallNum2 && Math.cos(difTime/timeDivider) > smallNum1)) {
+        //console.log(Math.cos(difTime/200));
+        footstepSoundEffect.play();
+        console.log(footstepSoundEffect.play());
+    }
 
     /*var tmpDist = 30;
     var tmpKot = 2.93;
